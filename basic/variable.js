a36155dc7bcb2d2fd4afc4a6a63414222cc24eae
Harshit1224556 <150434886+Harshit1224556@users.noreply.github.com>
2025-03-20 19:03:28 +0000

    container config done

---
+++ basic/variable.js
@@ -0,0 +1,9 @@
+const salary = 10
+var name = "Harshit"
+let id = 1234
+// salary=20
+name = "KKKKKK"
+id = 122222
+console.table([salary,name,id])
+
+// prefer not to use var due to its scope issue
