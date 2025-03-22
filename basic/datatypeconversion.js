657009ac00c03ef330ebe1f9e856de389244d018
Harshit1224556 <150434886+Harshit1224556@users.noreply.github.com>
2025-03-22 14:03:37 +0000

    container config done

---
+++ basic/datatypeconversion.js
@@ -0,0 +1,26 @@
+let a = "33aaaa"
+let b = Number(a)
+console.log(b)
+33 in string converts to 33 in number 
+33aa is string is not convert it into number so its gives the value NAN not a number
+// 
+ let a = true
+let b = Number(a)
+console.log(b)
+console.log(typeof b)
+let a = 1
+let b = Boolean(a)
+console.log(b)
+// console.log(typeof b)
+let num = 33
+let str = String(num)
+console.log(str)
+console.log(typeof str)
+
+str1 = "harshit"
+str2="sharma"
+str3=str1+str2
+console.log(str3)
+
+
+console.log("1" + 2 + 2)
