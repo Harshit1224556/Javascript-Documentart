443fe0aa7ada7bb4663513f93d5096c452c95dd3
Harshit Kumar <150434886+Harshit1224556@users.noreply.github.com>
2025-04-15 19:15:35 +0530

    Add files via upload

---
+++ basic/number.js
@@ -0,0 +1,38 @@
+// const a = 10
+// console.log(a)
+// const balance = new Number(100.0000000000000000)
+// console.log(balance)
+// console.log(balance.toString.length)
+// // precision
+// console.log(balance.toPrecision(2))
+
+
+// const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'))
+
+
+
+// Maths 
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(3.45))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.7))
+// console.log(Math.min(2,3,4,6))
+// console.log(Math.max(1,3,7,8))
+
+
+
+// important topic 
+// random function 
+
+// console.log(Math.random()) // ye 0 and 1 ke bich me value deta hah
+// console.log(Math.floor(Math.random()*10 + 1))
+
+// ab max and min value define kr looooo
+// and then go 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max-min+1) + min))
