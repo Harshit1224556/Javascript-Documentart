f0186a00105a40014a0a0fb097ab37f59e480118
Harshit Kumar <150434886+Harshit1224556@users.noreply.github.com>
2025-04-19 02:20:50 +0530

    Add files via upload

---
+++ basic/date.js
@@ -0,0 +1,35 @@
+// const mydate = new Date();
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate);
+
+//let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// // let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+
+// let mytimestamp = Date.now();
+// console.log(mytimestamp)
+
+// console.log(myCreatedDate.getTime())
+
+
+// console.log(newdate)
+// console.log(newdate.getDay())
+// console.log(newdate.getMonth()+1)
+// let newdate = new Date()
+// let weekday = newdate.toLocaleString('default', {
+//     weekday: "long"
+    
+// })
+
+// console.log(weekday)
+// console.log(newdate.toDateString())
+
+
+
+
+
+
